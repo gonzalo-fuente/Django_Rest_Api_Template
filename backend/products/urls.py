502a7ab4be131c9bc0dc3eb4efae2d5270a2fcbf
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-27 00:43:20 -0300

    Implement DRF ListAPIView and ListCreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_create_view),
+    path('list/', views.product_list_create_view),
     path('<int:pk>/', views.product_detail_view)
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -19,9 +19,34 @@ class ProductCreateAPIView(generics.CreateAPIView):
 product_create_view = ProductCreateAPIView.as_view()
 
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        # If there's no content I use the title as content
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk' ??
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductListAPIView(generics.ListAPIView):
+    '''
+    Not gonna use this method
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
