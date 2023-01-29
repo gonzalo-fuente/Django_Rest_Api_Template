4c1e983a11adb0baaa03ff96c9ca2a3fe45f64f5
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-28 23:46:13 -0300

    Implement function based views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.product_create_view),
-    path('list/', views.product_list_create_view),
-    path('<int:pk>/', views.product_detail_view)
+    # --- Class based views ---
+    # path('', views.product_create_view),
+    # path('list/', views.product_list_create_view),
+    # path('<int:pk>/', views.product_detail_view)
+
+    # --- Function based views ---
+    path('', views.product_alt_view),
+    path('<int:pk>/', views.product_alt_view)
 ]
