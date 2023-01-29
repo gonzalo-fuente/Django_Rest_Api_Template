77633e70c4033da5859980f353650ab2dbab78d6
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-29 16:52:09 -0300

    Implement search feature on JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,8 +1,17 @@
 const loginForm = document.getElementById("login-form");
+const contentContainer = document.getElementById("content-container");
+const searchForm = document.getElementById("search-form");
+
 const baseEndpoint = "http://localhost:8000/api";
+
 if (loginForm) {
   // handle this login form
   loginForm.addEventListener("submit", handleLogin);
+}
+
+if (searchForm) {
+  // handle this search form
+  searchForm.addEventListener("submit", handleSearch);
 }
 
 function handleLogin(event) {
@@ -22,10 +31,126 @@ function handleLogin(event) {
     .then((response) => {
       return response.json();
     })
-    .then((data) => {
-      console.log(data);
+    .then((authData) => {
+      handleAuthData(authData, getProductList);
     })
     .catch((err) => {
       console.log("err", err);
     });
 }
+
+function handleSearch(event) {
+  event.preventDefault();
+  let formData = new FormData(searchForm);
+  let data = Object.fromEntries(formData);
+  let searchParams = new URLSearchParams(data);
+  const endpoint = `${baseEndpoint}/search/?${searchParams}`;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const authToken = localStorage.getItem("access");
+  if (authToken) {
+    headers["Authorization"] = `Bearer ${authToken}`;
+  }
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
+  fetch(endpoint, options) //  Promise
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const validData = isTokenNotValid(data);
+      if (validData && contentContainer) {
+        contentContainer.innerHTML = "";
+        if (data && data.count) {
+          let htmlStr = "";
+          for (let result of data.results) {
+            htmlStr += "<li>" + result.title + "</li>";
+          }
+          contentContainer.innerHTML = htmlStr;
+          if (data.results.length === 0) {
+            contentContainer.innerHTML = "<p>No results found</p>";
+          }
+        } else {
+          contentContainer.innerHTML = "<p>No results found</p>";
+        }
+      }
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
+
+function handleAuthData(authData, callback) {
+  localStorage.setItem("access", authData.access);
+  localStorage.setItem("refresh", authData.refresh);
+  if (callback) {
+    callback();
+  }
+}
+
+function writeToContainer(data) {
+  if (contentContainer) {
+    contentContainer.innerHTML =
+      "<pre>" + JSON.stringify(data, null, 4) + "</pre>";
+  }
+}
+
+function getFetchOptions(method, body) {
+  return {
+    method: method === null ? "GET" : method,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem("access")}`,
+    },
+    body: body ? body : null,
+  };
+}
+
+function isTokenNotValid(jsonData) {
+  if (jsonData.code && jsonData.code === "token_not_valid") {
+    // run a refresh token fetch
+    alert("Please login again");
+    return false;
+  }
+  return true;
+}
+
+function validateJWTToken() {
+  // fetch
+  const endpoint = `${baseEndpoint}/token/verify/`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("access"),
+    }),
+  };
+  fetch(endpoint, options)
+    .then((response) => response.json())
+    .then((data) => {
+      // refresh token
+    });
+}
+
+function getProductList() {
+  const endpoint = `${baseEndpoint}/products/`;
+  const options = getFetchOptions();
+  fetch(endpoint, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const validData = isTokenNotValid(data);
+      if (validData) {
+        writeToContainer(data);
+      }
+    });
+}
+
+validateJWTToken();
+getProductList();
