c9551c5f641bd797afcc1994f0b243afac0ad1bb
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-29 00:02:49 -0300

    Implement Update and Delete views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     # --- Class based views ---
-    # path('', views.product_create_view),
-    # path('list/', views.product_list_create_view),
-    # path('<int:pk>/', views.product_detail_view)
+    path('', views.product_list_create_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/delete/', views.product_destroy_view),
+    path('<int:pk>/', views.product_detail_view)
 
     # --- Function based views ---
-    path('', views.product_alt_view),
-    path('<int:pk>/', views.product_alt_view)
+    # path('', views.product_alt_view),
+    # path('<int:pk>/', views.product_alt_view)
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -57,30 +57,54 @@ class ProductListAPIView(generics.ListAPIView):
 product_list_view = ProductListAPIView.as_view()
 
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
+
+
 # --- Function based views ---
-@api_view(['GET', 'POST'])
-def product_alt_view(request, pk=None, *args, **kwargs):
-    method = request.method 
+# @api_view(['GET', 'POST'])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method = request.method 
 
-    if method == "GET":
-        if pk is not None:
-            # detail view
-            obj = get_object_or_404(Product, pk=pk)
-            data = ProductSerializer(obj, many=False).data
-            return Response(data)
-        # list view
-        queryset = Product.objects.all() 
-        data = ProductSerializer(queryset, many=True).data
-        return Response(data)
+#     if method == "GET":
+#         if pk is not None:
+#             # detail view
+#             obj = get_object_or_404(Product, pk=pk)
+#             data = ProductSerializer(obj, many=False).data
+#             return Response(data)
+#         # list view
+#         queryset = Product.objects.all() 
+#         data = ProductSerializer(queryset, many=True).data
+#         return Response(data)
 
-    if method == "POST":
-        # create an item
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content') or None
-            if content is None:
-                content = title
-            serializer.save(content=content)
-            return Response(serializer.data)
-        return Response({"invalid": "not good data"}, status=400)
+#     if method == "POST":
+#         # create an item
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             title = serializer.validated_data.get('title')
+#             content = serializer.validated_data.get('content') or None
+#             if content is None:
+#                 content = title
+#             serializer.save(content=content)
+#             return Response(serializer.data)
+#         return Response({"invalid": "not good data"}, status=400)
