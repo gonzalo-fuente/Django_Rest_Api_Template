1f7b7f29b2b5e5b4c61b2ab8b8fcd02df2067b51
Gonzalo <gonzalo_a_fuente@hotmail.com>
2023-01-29 00:45:20 -0300

    Implement permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
+from rest_framework import authentication, generics, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 
@@ -27,6 +28,8 @@ product_create_view = ProductCreateAPIView.as_view()
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get('title')
